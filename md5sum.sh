d47b4fe8cb65790ccaada8dba8af03ec4b3402ef
Tomas Podermanski <tpoder@cis.vutbr.cz>
2013-01-12 19:00:56 +0000

    Import of TPODER/Net-NfDump-0.01_03 from CPAN.
    gitpan-cpan-distribution: Net-NfDump
    gitpan-cpan-version:      0.01_03
    gitpan-cpan-path:         TPODER/Net-NfDump-0.01_03.tar.gz
    gitpan-cpan-author:       TPODER
    gitpan-cpan-maturity:     developer

--- md5sum.sh
+++ md5sum.sh
@@ -5,7 +5,9 @@
 SUM=$1
 FILE=$2
 
-if [ -x /sbin/md5 ] ; then 
+if [ -x /bin/md5 ] ; then 
+	RES=$(/bin/md5 $FILE)
+elif [ -x /sbin/md5 ] ; then 
 	RES=$(/sbin/md5 $FILE)
 elif [ -x /usr/bin/md5sum ] ; then 
 	RES=$(/usr/bin/md5sum $FILE)
@@ -15,15 +17,17 @@ elif [ -x /usr/local/bin/openssl ] ; then
 	RES=$(/usr/local/bin/openssl dgst -md5 $FILE)
 else 
 	echo "No MD5 checksum utility found"
+	uname -a 
 	exit 1
 fi 
 
-if [ "$(echo $RES | grep  $SUM)" == "" ] ; then 
+if [ "$(echo $RES | grep  $SUM)" != "" ] ; then 
+	echo "Checksum for $FILE is OK"
+	exit 0
+else 
 	echo "Invalid checksum for $FILE"
 	echo "Excpected: $SUM"
 	echo "Result: $RES"
 	exit 1;
-else 
-	echo "Checksum for $FILE is OK"
-	exit 0
 fi 
+
